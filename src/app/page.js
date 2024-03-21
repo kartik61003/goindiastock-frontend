2128f736d69b51c8293235c358b4b225afbd34b3
kartik61003 <kartik61003@gmail.com>
2024-03-21 23:44:24 +0530

    fixing ladingPAGE

--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,10 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import Discussion from "./component/Discussion";
-import Navbar from "./component/Navbar";
-import Sidebar from "./component/Sidebar";
-import Stories from "./component/Stories";
-import "./globals.css";
+import LandingPage from "./LandingPage";
 
 const page = () => {
-  const [activeComponent, setActiveComponent] = useState("discussion");
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768); // Adjust breakpoint as needed
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    setActiveComponent("discussion");
-  }, [isMobile]);
 
   return (
     <>
-    {isMobile ? (
-      <Sidebar/>
-    ):
-    (
-
-      <></>
-    )
-    }
-
-      <Navbar
-        activeComponent={activeComponent}
-        setActiveComponent={setActiveComponent}
-      />
-
-      <div>
-        {isMobile ? (
-          // On mobile, display only the active component
-          <div className="pt-10">
-            {activeComponent === "discussion" && <Discussion />}
-            {activeComponent === "stories" && <Stories />}
-          </div>
-        ) : (
-          // On desktop, display both components side by side
-          <div className="flex flex-row justify-between ">
-            <Sidebar/>
-            <Discussion />
-            <Stories />
-          </div>
-        )}
-      </div>
+      <LandingPage/>
     </>
   );
 };
